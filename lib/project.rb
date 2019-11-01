578e9c545e3f5ef9c1d1608c26d8dc4d7ae37a8f
Will Drougas <wdrougas@gmail.com>
2019-11-01 12:03:56 -0400

    Completed Lab

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require_relative "backer.rb"
+
+class Project
+
+    attr_reader :title, :backers
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @backers = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_backer(backer)
+        backer.backed_projects << self
+        @backers << backer
+    end
+
+end
